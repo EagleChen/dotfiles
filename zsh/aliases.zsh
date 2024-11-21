9314d4aecfde3779bb97d1f37d69c585d3ca3769
Eagle Chen <chygr1234@gmail.com>
2024-11-21 12:12:44 +0000

    feat: add zsh config

---
+++ zsh/aliases.zsh
@@ -0,0 +1,4 @@
+alias gst='git status'
+alias ggpush='git push origin "$(git_branch_current 2> /dev/null)"'
+alias ggpull='git pull origin "$(git_branch_current 2> /dev/null)"'
+alias z='fasd_cd -d'

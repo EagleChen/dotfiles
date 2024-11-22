5df25aa799c3c6e5018ecaa843504157651f0508
Eagle Chen <chygr1234@gmail.com>
2024-11-22 15:55:28 +0800

    Update aliases.zsh

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,4 +1,5 @@
 alias gst='git status'
 alias ggpush='git push origin "$(git_branch_current 2> /dev/null)"'
 alias ggpull='git pull origin "$(git_branch_current 2> /dev/null)"'
+alias gd='git diff'
 alias z='fasd_cd -d'
